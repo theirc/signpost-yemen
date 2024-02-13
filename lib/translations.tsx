08debe32322dc8f763676460bf7e39f4c350fee7
Bianca Chami <b.chami39@gmail.com>
2024-02-13 16:48:49 +0000

    feat: added translation

--- lib/translations.tsx
+++ lib/translations.tsx
@@ -182,6 +182,7 @@ export function populateServiceMapStrings(dynamicContent: {
     labelPopulations: dynamicContent['default_populations_label'],
     labelProvider: dynamicContent['default_providers_label'],
     labelServicesTypes: dynamicContent['default_service_type_label'],
+    searchListString: dynamicContent['default_search_hint'],
   };
 }
 
